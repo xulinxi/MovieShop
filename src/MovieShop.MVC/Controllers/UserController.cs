97a9554f466afe8c868c8dff8a72dcac77868b5b
reddy57 <abhilash.reddy@outlook.com>
2021-02-23 21:33:20 -0500

    Buy Movie Page

--- src/MovieShop.MVC/Controllers/UserController.cs
+++ src/MovieShop.MVC/Controllers/UserController.cs
@@ -1,16 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using ApplicationCore.Models;
+using ApplicationCore.ServiceInterfaces;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MovieShop.MVC.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index()
+        private readonly IMovieService _movieService;
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService, IMovieService movieService)
         {
-            return View();
+            _userService = userService;
+            _movieService = movieService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BuyMovie(int id)
+        {
+            var movie = await _movieService.GetMovieAsync(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> BuyMovie(PurchaseRequestModel purchase)
+        {
+            await _userService.PurchaseMovie(purchase);
+            return Ok();
         }
     }
 }
